9be1ad22a4a0428fca8192270407a9c63b7ac986
Bagrat Aznauryan <contact@bagrat.io>
2022-11-02 04:31:00 +0400

    Add Question.position

--- lib/quizy/quizes/question.ex
+++ lib/quizy/quizes/question.ex
@@ -7,20 +7,30 @@ defmodule Quizy.Quizes.Question do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "questions" do
-    field :multiple_choice?, :boolean, default: false, source: :multiple_choice
     field :text, :string
+    field :position, :integer
+    field :multiple_choice?, :boolean, default: false, source: :multiple_choice
 
-    belongs_to :quize, Quizy.Quizes.Quiz
+    belongs_to :quiz, Quizy.Quizes.Quiz
 
     timestamps()
   end
 
   @doc false
-  def changeset(question, attrs) do
+  def create_changeset(question, attrs) do
     attrs = Repo.rename_bool_attrs(attrs, ["multiple_choice"])
 
     question
-    |> cast(attrs, [:text, :multiple_choice?])
-    |> validate_required([:text, :multiple_choice?])
+    |> cast(attrs, [:quiz_id, :text, :position, :multiple_choice?])
+    |> validate_required([:quiz_id, :text, :position, :multiple_choice?])
+  end
+
+  @doc false
+  def update_changeset(question, attrs) do
+    attrs = Repo.rename_bool_attrs(attrs, ["multiple_choice"])
+
+    question
+    |> cast(attrs, [:text, :position, :multiple_choice?])
+    |> validate_required([:text, :position, :multiple_choice?])
   end
 end
--- priv/repo/migrations/20221101134220_create_questions.exs
+++ priv/repo/migrations/20221101134220_create_questions.exs
@@ -4,13 +4,15 @@ defmodule Quizy.Repo.Migrations.CreateQuestions do
   def change do
     create table(:questions, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :text, :string
+      add :text, :string, null: false
       add :multiple_choice, :boolean, default: false, null: false
+      add :position, :integer, null: false
+
       add :quiz_id, references(:quizes, on_delete: :nothing, type: :binary_id)
 
       timestamps()
     end
 
-    create index(:questions, [:quiz_id])
+    create index(:questions, [:quiz_id, :position])
   end
 end
